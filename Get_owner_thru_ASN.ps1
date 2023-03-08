f9e8c0038c33b6e75ff0742057b27e44d4e3ab9f
WolfhoundDev <39737315+Wolfhounds227@users.noreply.github.com>
2023-03-08 10:14:25 -0600

    Add files via upload
    Currently only works for the RIPE registrar (Réseaux IP Européens Network Coordination Centre)  Middle east, Europe and some parts of Asia. Need to design some logic to check against all registrar's

---
+++ Get_owner_thru_ASN.ps1
@@ -0,0 +1,49 @@
+﻿<# Non-interactive entry
+$ipAddresses = @("205.156.141.241", "167.245.92.242", "8.8.8.8")
+#>
+# Only works for RIPE Regional
+# Interactive entry
+Write-Host "Please enter a list of IP addresses separated by a comma: "
+$ipAddresses = Read-Host
+$ipAddresses = $ipAddresses -split ","
+
+Function Get-ASN {
+    Param ($ipAddress)
+
+    $url = "https://stat.ripe.net/data/network-info/data.json?resource=$ipAddress"
+    $response = Invoke-WebRequest -Uri $url
+
+    If ($response.StatusCode -eq 200) {
+        $data = $response.Content | ConvertFrom-Json
+        return $data.data.asns[0]
+    }
+    else {
+        return $null
+    }
+}
+
+Function Get-CompanyName {
+    Param ($asn)
+
+    $url = "https://stat.ripe.net/data/as-overview/data.json?resource=$asn"
+    $response = Invoke-WebRequest -Uri $url
+
+    If ($response.StatusCode -eq 200) {
+        $data = $response.Content | ConvertFrom-Json
+        return $data.data.holder
+    }
+    else {
+        return $null
+    }
+}
+
+Foreach ($ipAddress in $ipAddresses) {
+    $asn = Get-ASN $ipAddress
+    If ($asn -ne $null) {
+        $companyName = Get-CompanyName $asn
+        Write-Host "$ipAddress belongs to $companyName"
+    }
+    else {
+        Write-Host "$ipAddress is not associated with an ASN"
+    }
+}
